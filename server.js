c59213247842ca390a25dc4ab9fb60f0278af48c
Suraj18Dev <suraj.singh.18.engineer@gmail.com>
2025-07-19 20:16:34 +0530

    4th

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from new Node.js App!\n');
+  res.end('<h1>This is suraj !!!!!Hello from new Node.js App!</h1>\n');
 });
 
 server.listen(PORT, () => {
